88336acd2abe52851ebfec07f3a9bd57d7735998
Tao <136265333@qq.com>
2019-01-04 09:50:02 +0800

    把本地工程添加到git

---
+++ git-test-project/src/cn/itcast/demo.java
@@ -0,0 +1,10 @@
+package cn.itcast;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
